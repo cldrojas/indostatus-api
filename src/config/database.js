631a3e1d7b2e2422a91b2c3483679151eb34a512
Daniel Rojas <cld.rojasz@gmail.com>
2021-12-21 20:57:56 -0300

    add enviroment variables for database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
-const config = {}
+const config = {};
 
-config.host = 'localhost';
-config.user = 'root';
-config.password = '';
-config.database = 'lefufu';
+config.host = process.env.DATABASE_HOST;
+config.user = process.env.DATABASE_USER;
+config.password = process.env.DATABASE_PASSWORD;
+config.database = process.env.DATABASE_NAME;
 
 module.exports = config;
